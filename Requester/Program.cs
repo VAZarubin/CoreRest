2146506dd5792721270c848947ce5a42bd1e09f0
VZarubin <vzarubin@luxoft.com>
2019-06-03 12:10:47 +0600

    create

---
+++ Requester/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Flurl.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Requester
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CancellationTokenSource token = new CancellationTokenSource();
+            
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(10));
+                    
+                    if (token.IsCancellationRequested)
+                    {
+                        return;
+                    }
+                    
+                    dynamic req = await "http://localhost:5000/api/Some/Some".GetJsonAsync();
+
+                    string rr = JsonConvert.SerializeObject((object)req);
+                    
+                    Console.WriteLine(rr);
+
+                }
+
+
+            }, token.Token);
+            
+            Console.WriteLine("Press any key to Exit");
+
+            Console.Read();
+            
+            token.Cancel();
+        }
+    }
+}
